4d027f7ddb188aa060b7a1bca58c7d2896f8c323
Gustavo-Lozada <119980944+Gustavo-Lozada@users.noreply.github.com>
2023-07-22 22:35:08 -0500

    Update Config.php

--- Config/Config.php
+++ Config/Config.php
@@ -3,6 +3,6 @@ const base_url = "http://localhost:3000/";
 const host = "localhost";
 const user = "root";
 const pass = "";
-const db = "biblioteca";
+const db = "byxvubda8oa9tsvojcex";
 const charset = "charset=utf8";
 ?>
