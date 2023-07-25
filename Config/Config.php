62be2c1798963fe860655db5140d00299fc909c0
Gustavo-Lozada <119980944+Gustavo-Lozada@users.noreply.github.com>
2023-07-25 00:32:46 -0500

    Update Config.php

--- Config/Config.php
+++ Config/Config.php
@@ -1,5 +1,5 @@
 <?php
-const host = "byxvubda8oa9tsvojcex-mysql.services.clever-cloud.com";
+const host = "https://app-d28c1b93-9c2e-48e7-a29e-db3fddbbc74e.cleverapps.io/";
 const user = "ufqaub7vz9kqxylj";
 const pass = "VLj4d0tudmn4HDvRYdu5";
 const db = "byxvubda8oa9tsvojcex";
